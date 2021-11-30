6c401eb0a3a59dc5c343d74ecb7adb7c13e206c8
Vishnu Kay Vee <55887197+vishnu-k-v@users.noreply.github.com>
2021-11-30 20:51:28 +0530

    added build

--- src/app/Pages/addperson/addperson.component.ts
+++ src/app/Pages/addperson/addperson.component.ts
@@ -8,38 +8,35 @@ import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-addperson',
   templateUrl: './addperson.component.html',
-  styleUrls: ['./addperson.component.css']
+  styleUrls: ['./addperson.component.css'],
 })
 export class AddpersonComponent implements OnInit {
+  myDate = new Date();
+  day: string;
+  month: string;
+  year: string;
+  constructor(private http: HttpClient, private datepipe: DatePipe) {}
 
-  myDate=new Date();
-  day:string;
-  month:string;
-  year:string;
-  constructor(private http:HttpClient,private datepipe:DatePipe) { 
-   
-  }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  SavePerson(personData: {
+    name: string;
+    age: string;
+    address: string;
+    bloodgroup: string;
+    pincode: string;
+    mobile: string;
+    donationdate: string;
+  }) {
+    this.month = (this.myDate.getMonth() + 1).toString();
+    this.day = this.myDate.getUTCDate().toString();
+    this.year = this.myDate.getFullYear().toString();
+    personData.donationdate = this.day + '-' + this.month + '-' + this.year;
 
-  SavePerson(personData:{name:string;
-  age:string;
-  address:string;
-  bloodgroup:string;
-  pincode:string;
-  mobile:string;
-  donationdate:string;
-  }){
-    
-     this.month=(this.myDate.getMonth()+1).toString();
-     this.day=this.myDate.getUTCDate().toString();
-     this.year=this.myDate.getFullYear().toString();
-    personData.donationdate=this.day+"-"+this.month+"-"+this.year;
-    
-    this.http.post(environment.firebase.databaseURL+"/person.json",personData).subscribe(Response=>{
-console.log(Response);
-    });
-
+    this.http
+      .post(environment.firebase.databaseURL + '/person.json', personData)
+      .subscribe((Response) => {
+        console.log(Response);
+      });
   }
 }
